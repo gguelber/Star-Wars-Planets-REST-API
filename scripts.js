81ac9bb862bf3e8105ea1d9319b4358c9d3906de
Gustavo <gguelber@yahoo.com.br>
2019-05-22 12:30:18 -0300

    Update package.json, refactored scripts.js code to use only async await

--- scripts.js
+++ scripts.js
@@ -3,24 +3,22 @@ const population = document.getElementById('pop');
 const climate = document.getElementById('climate');
 const terrain = document.getElementById('terrain');
 const films = document.getElementById('films');
-const url = 'http://localhost:3000/planets';
+const endpoint = 'http://localhost:3000/planets';
 
-const getRandomPlanet = async () => {
-	await fetch(url)
-		.then((response) => {
-			const data = response.json();
-			return data;
-		})
-		.then((result) => {
-			let randomNumber = Math.floor(Math.random() * result.length);
-			//console.log(randomNumber);
-			planetName.innerHTML = `${result[randomNumber].name}`;
-			population.innerHTML = `Population: ${result[randomNumber].population}`;
-			climate.innerHTML = `Climate: ${result[randomNumber].climate}`;
-			terrain.innerHTML = `Terrain: ${result[randomNumber].terrain}`;
-			films.innerHTML = `Featured in ${result[randomNumber].films.length} films`;
-			return result;
-		});
-};
+let data = {};
+async function getData(url) {
+	try {
+		const response = await fetch(url);
+		const result = await response.json();
+		let randomNumber = Math.floor(Math.random() * result.length);
+		planetName.innerHTML = `${result[randomNumber].name}`;
+		population.innerHTML = `Population: ${result[randomNumber].population}`;
+		climate.innerHTML = `Climate: ${result[randomNumber].climate}`;
+		terrain.innerHTML = `Terrain: ${result[randomNumber].terrain}`;
+		films.innerHTML = `Featured in ${result[randomNumber].films.length} films`;
+	} catch (error) {
+		window.alert(error);
+	}
+}
 
-window.onload = getRandomPlanet();
+window.onload = getData(endpoint);
